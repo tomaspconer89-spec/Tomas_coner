9f3b8060c8fe07d54c476ec6eae1967fea0769e2
Tomas <tomaspconer89@gmail.com>
2026-03-04 18:32:37 -0500

    Week 2 Day 2 - File I/O user input and save to users.txt

--- 2_weeks_1-day/week2_day1.py
+++ 2_weeks_1-day/week2_day1.py
@@ -17,16 +17,3 @@ class User:
         else:
             return intro + " I am a minor."
     
-name = input("Enter your name: ")
-city = input("Enter your city: ")
-try:
-    age = int(input("Enter your age:  "))
-except ValueError:
-    print("Invalid input for age. Please enter a number.")
-    age = 0
-    
-user = User(name, city, age)
-
-with open("users.txt", "a") as file:
-    file.write(user.introduce() + "\n")
-    print("User saved to users.txt successfully.")
